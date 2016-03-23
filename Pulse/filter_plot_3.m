e65372b8e773fb12e307d7ca2b329e98bc93980f
chris.hoen <chris.hoen@verizon.net>
2016-03-23 13:50:38 -0400

    filter plot

--- Pulse/filter_plot_3.m
+++ Pulse/filter_plot_3.m
@@ -1,6 +1,6 @@
 function filter_plot_3
 
-x = load('C:\MyLib\Data\FilterMovingAverage.csv');
+x = load('C:\MyLib\Data\FilterAlphaTwo.csv');
 
 ax1 = subplot(3,1,1); % top    subplot X
 ax2 = subplot(3,1,2); % middle subplot EX
@@ -13,7 +13,7 @@ plot(ax2,x(:,2),x(:,4))
 axis(ax2,[0 inf -2.0 2.0])
 
 plot(ax3,x(:,2),x(:,5))
-axis(ax3,[0 inf 0.0 2.0])
+axis(ax3,[0 inf -2.0 12.0])
 
 h=zoom;
 set(h,'Motion','horizontal','Enable','on');

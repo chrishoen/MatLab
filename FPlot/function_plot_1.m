bb6107d78ce99d253b00d5a354889ce65fcb7cbd
chris.hoen <chris.hoen@verizon.net>
2016-05-14 00:37:00 -0400

    more

---
+++ FPlot/function_plot_1.m
@@ -0,0 +1,14 @@
+function function_plot_1
+fplot(@f101,[-1 1])
+return
+
+function y = f101(t)
+
+y = atanh(t);
+
+return
+
+
+
+
+
---
+++ FPlot/function_plot_2.m
@@ -0,0 +1,20 @@
+function function_plot_2
+ezplot(@fx,@fy,[-1 1])
+return
+
+function x = fx(t)
+
+x = cosh(t);
+
+return
+
+function y = fy(t)
+
+y = sinh(t);
+
+return
+
+
+
+
+

bcf77055417c559f58ea0e87bdf7d7ab7d0cc39c
chrishoen <chris.hoen@red-inc.us>
2016-06-26 12:30:41 -0400

    more

---
+++ AlphaFilter/filter_plot_AlphaStatistics.m
@@ -0,0 +1,22 @@
+function filter_plot_AlphaTwo
+
+x = load('C:\Alpha\Data\FilterAlphaStatistics.csv');
+
+ax1 = subplot(3,1,1); % top    subplot X
+ax2 = subplot(3,1,2); % middle subplot EX
+ax3 = subplot(3,1,3); % bottom subplot UX
+
+plot(ax1,x(:,2),x(:,3))
+axis(ax1,[0 inf -2.0 2.0])
+
+plot(ax2,x(:,2),x(:,4))
+axis(ax2,[0 inf -2.0 2.0])
+
+plot(ax3,x(:,2),x(:,5))
+axis(ax3,[0 inf -2 2])
+
+h=zoom;
+set(h,'Motion','horizontal','Enable','on');
+
+return
+

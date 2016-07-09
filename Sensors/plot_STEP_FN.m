660c8211d655376a6213444dfdf42ccabb31c2ec
chrishoen <chris.hoen@red-inc.us>
2016-07-08 20:27:30 -0400

    more

--- Sensors/plot_STEP_FN.m
+++ Sensors/plot_STEP_FN.m
@@ -1,8 +1,8 @@
 function filter_plot_SampleTest
 
-x = load('C:\Alpha\Data\STEP_F1_102.csv');
-plot(x(:,2),x(:,3))
-axis([-inf inf -6.0 2.0])
+x = load('C:\Alpha\Data\STEP_F1_101.csv');
+plot(x(:,2),-x(:,3))
+axis([-inf inf -2.0 6.0])
 h=zoom;
 set(h,'Motion','horizontal','Enable','on');
 
---
+++ Sensors/stepinfo_STEP_FN.m
@@ -0,0 +1,8 @@
+function stepinfo_STEP_FN
+
+x = load('C:\Alpha\Data\STEP_F1_101.csv');
+s = stepinfo(x(:,3),-x(:,2),-4.7)
+
+
+return
+

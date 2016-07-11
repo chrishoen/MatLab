a608d6a8162e41ce93b35325910170cb885a6ed9
chrishoen <chris.hoen@red-inc.us>
2016-07-10 20:28:25 -0400

    plot_STEP_FN.m

--- Sensors/plot_STEP_FN.m
+++ Sensors/plot_STEP_FN.m
@@ -1,11 +1,12 @@
 function filter_plot_SampleTest
 
-x = load('C:\Alpha\Data\STEP_F1_101.csv');
+x = load('C:\Alpha\Data\STEP_F9_102.csv');
 plot(x(:,2),-x(:,3))
 axis([-inf inf -2.0 6.0])
 h=zoom;
 set(h,'Motion','horizontal','Enable','on');
 
+s = stepinfo(x(:,3),-x(:,2),-4.875)
 
 return
 

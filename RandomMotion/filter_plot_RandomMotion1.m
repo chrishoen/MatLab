f9d9b345659be5812938baa66a608d4bafbafa60
chrishoen <chris.hoen@red-inc.us>
2016-09-07 09:41:18 -0400

    more

--- RandomMotion/filter_plot_RandomMotion1.m
+++ RandomMotion/filter_plot_RandomMotion1.m
@@ -1,8 +1,8 @@
 function filter_plot_RandomMotion1
 
 x = load('C:\Alpha\Data\RandomMotion.csv');
-plot(x(:,2),x(:,3))
-axis([0 inf -10.0 10.0])
+plot(x(:,2),x(:,4))
+axis([0 inf -2.0 2.0])
 h=zoom;
 set(h,'Motion','horizontal','Enable','on');
 

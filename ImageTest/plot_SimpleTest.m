fb48a9eae2c118ea2066a8c15161e77483b26ef6
chrishoen <chris.hoen@red-inc.us>
2016-12-10 19:26:23 -0500

    more

---
+++ ImageTest/plot_SimpleTest.m
@@ -0,0 +1,18 @@
+function plot_SimpleTest
+
+x = load('C:\Alpha\Data\SimpleTest.csv');
+
+ax1 = subplot(2,1,1); % top subplot
+ax2 = subplot(2,1,2); % bottom subplot
+
+plot(ax1,x(:,1),x(:,3))
+axis(ax1,[0 inf -2.0 12.0])
+
+plot(ax2,x(:,2),x(:,4))
+axis(ax2,[0 inf -2.0 2.0])
+
+h=zoom;
+set(h,'Motion','horizontal','Enable','on');
+
+return
+

f6436aeefb079f7808cbc168b5ffc8269b5fe4d4
chrishoen <chris.hoen@red-inc.us>
2016-12-10 19:38:50 -0500

    more

--- ImageTest/plot_SimpleTest.m
+++ ImageTest/plot_SimpleTest.m
@@ -5,7 +5,7 @@ x = load('C:\Alpha\Data\SimpleTest.csv');
 ax1 = subplot(2,1,1); % top subplot
 ax2 = subplot(2,1,2); % bottom subplot
 
-plot(ax1,x(:,1),x(:,3))
+plot(ax1,x(:,2),x(:,3))
 axis(ax1,[0 inf -2.0 12.0])
 
 plot(ax2,x(:,2),x(:,4))

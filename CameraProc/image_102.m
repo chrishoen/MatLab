fef6bf727b3537f7425ff7043ba6905b1d61cfd0
chrishoen <chris.hoen@red-inc.us>
2016-12-29 13:53:37 -0500

    SDSAT-740 #comment added gaussian filter

--- CameraProc/image_102.m
+++ CameraProc/image_102.m
@@ -1,19 +1,26 @@
 %***********************************************************BEGIN
 function image_101
 
-X1 = imread('C:\Alpha\Image\SubImageFiltered.jpg');
+X1 = imread('C:\Alpha\Image\SubImage_1.jpg');
 %size(X1)
 %imshow(X1)
 %imhist(X1)
 
-figure
-surf(X1)
+%surf(X1)
 
 Rmin = 5;
-Rmax = 14;
+Rmax = 16;
 
 [centers, radii] = imfindcircles(X1,[Rmin Rmax])
 
+X2 = imgaussfilt(X1,2);
+figure
+surf(X2)
+
+Rmin = 5;
+Rmax = 16;
+
+[centers, radii] = imfindcircles(X2,[Rmin Rmax])
 
 return
 %***********************************************************END

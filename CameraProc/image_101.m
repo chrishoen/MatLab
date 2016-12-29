193008c9b669d6280e8afeabfd5c6090654a444d
chrishoen <chris.hoen@red-inc.us>
2016-12-29 12:36:22 -0500

    more

--- CameraProc/image_101.m
+++ CameraProc/image_101.m
@@ -1,18 +1,26 @@
 %***********************************************************BEGIN
 function image_101
 
-I = imread('C:\Alpha\Image\SubImage_1.jpg');
-%size(I)
-%imshow(I)
-%imhist(I)
+X1 = imread('C:\Alpha\Image\SubImage_1.jpg');
+%size(X1)
+%imshow(X1)
+%imhist(X1)
 
-surf(I)
+%surf(X1)
 
 Rmin = 5;
-Rmax = 15;
+Rmax = 16;
 
-[centers, radii] = imfindcircles(I,[Rmin Rmax])
+[centers, radii] = imfindcircles(X1,[Rmin Rmax])
 
+X2 = imgaussfilt(X1,2);
+figure
+surf(X2)
+
+Rmin = 5;
+Rmax = 16;
+
+[centers, radii] = imfindcircles(X2,[Rmin Rmax])
 
 return
 %***********************************************************END

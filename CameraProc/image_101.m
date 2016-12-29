281d86dc9a5da5f0c77e2772e0d8ffb493ce4ec1
chrishoen <chris.hoen@red-inc.us>
2016-12-28 21:39:51 -0500

    more

---
+++ CameraProc/image_101.m
@@ -0,0 +1,14 @@
+%***********************************************************BEGIN
+function image_101
+
+I = imread('C:\Alpha\Image\SubImage_1.jpg');
+size(I)
+%imshow(I)
+%imhist(I)
+
+surf(I)
+
+return
+%***********************************************************END
+
+

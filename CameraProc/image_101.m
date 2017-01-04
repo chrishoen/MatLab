0e61ee534a0fd1e63ddef81b161cbdb079116cdf
chrishoen <chris.hoen@red-inc.us>
2017-01-04 11:16:32 -0500

    more

--- CameraProc/image_101.m
+++ CameraProc/image_101.m
@@ -7,9 +7,11 @@ X1 = imread('C:\Alpha\Image\SubImage_1.jpg');
 %imhist(X1)
 
 fig1 = figure;
-surf(X1)
+%surf(X1)
+%contour3(X1)
+surf(X1,'linespec','none')
 %print(fig1,'C:\Alpha\Image\SubImage_1','-dpng')
-
+return 
 Rmin = 5;
 Rmax = 14;
 

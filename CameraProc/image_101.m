4fc4210bddf38f30196745229ad2388ff1c3b109
chrishoen <chris.hoen@red-inc.us>
2017-01-04 12:57:35 -0500

    more

--- CameraProc/image_101.m
+++ CameraProc/image_101.m
@@ -1,7 +1,8 @@
 %***********************************************************BEGIN
 function image_101
 
-X1 = imread('C:\Alpha\Image\SubImage_1.jpg');
+X1 = imread('C:\Alpha\Image\SubImageFiltered.jpg');
+%X1 = imread('C:\Alpha\Image\SubImageFiltered.jpg');
 %size(X1)
 %imshow(X1)
 %imhist(X1)
@@ -9,7 +10,7 @@ X1 = imread('C:\Alpha\Image\SubImage_1.jpg');
 fig1 = figure;
 %surf(X1)
 %contour3(X1)
-surf(X1,'linespec','none')
+surf(X1)
 %print(fig1,'C:\Alpha\Image\SubImage_1','-dpng')
 return 
 Rmin = 5;

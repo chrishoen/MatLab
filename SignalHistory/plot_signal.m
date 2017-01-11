06037b857504173c72003b590e37ec24df7d4604
chrishoen <chris.hoen@red-inc.us>
2017-01-11 18:52:09 -0500

    more

--- SignalHistory/plot_signal.m
+++ SignalHistory/plot_signal.m
@@ -1,7 +1,7 @@
 function plot_signal
 
 x = load('C:\Alpha\Data\SignalHistory.csv');
-plot(x(:,2),x(:,3))
+plot(x(:,2),x(:,4))
 axis([0 inf -10.0 10.0])
 h=zoom;
 set(h,'Motion','horizontal','Enable','on');

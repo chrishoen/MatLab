5fda85f0ccfb899589259a50abab02b49efb221f
chrishoen <chris.hoen@red-inc.us>
2017-01-12 10:25:44 -0500

    more

--- SignalHistory/plot_signal.m
+++ SignalHistory/plot_signal.m
@@ -6,7 +6,7 @@ hold on
 plot(x(:,2),x(:,3));
 plot(x(:,2),x(:,4));
 %axis([1.0 1.2 -10.0 10.0])
-eaxis([0 inf -10.0 10.0])
+axis([0 inf -10.0 10.0])
 h=zoom;
 set(h,'Motion','horizontal','Enable','on');
 

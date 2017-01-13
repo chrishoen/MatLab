ebedbfda1683c619b66d1c17a3e8a9c181964267
chrishoen <chris.hoen@red-inc.us>
2017-01-13 14:07:27 -0500

    more

--- HistoryTest/plot_history.m
+++ HistoryTest/plot_history.m
@@ -1,10 +1,10 @@
-function plot_signal
+function plot_history
 
 x = load('C:\Alpha\Data\SignalHistory.csv');
 figure 
 hold on
 plot(x(:,2),x(:,3));
-%plot(x(:,2),x(:,4));
+plot(x(:,2),x(:,4));
 %axis([1.0 1.2 -10.0 10.0])
 axis([0 inf -10.0 10.0])
 h=zoom;

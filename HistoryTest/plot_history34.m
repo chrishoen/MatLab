d95c00ffaf183823027c965180e27951117fe684
chrishoen <chris.hoen@red-inc.us>
2017-01-30 14:25:17 -0500

    more

---
+++ HistoryTest/plot_history34.m
@@ -0,0 +1,15 @@
+function plot_history34
+
+x = load('C:\Alpha\Data\SignalHistory.csv');
+figure 
+hold on
+plot(x(:,2),x(:,3));
+plot(x(:,2),x(:,4));
+%axis([1.0 1.2 -10.0 10.0])
+axis([0 inf -10.0 10.0])
+h=zoom;
+set(h,'Motion','horizontal','Enable','on');
+
+
+return
+

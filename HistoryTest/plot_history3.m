b0bdcfc853c241740c1089980147cee54d2e44bd
chrishoen <chris.hoen@red-inc.us>
2017-02-01 18:47:57 -0500

    more

--- HistoryTest/plot_history3.m
+++ HistoryTest/plot_history3.m
@@ -1,9 +1,9 @@
-efunction plot_history3
+function plot_history3
 
 x = load('C:\Alpha\Data\SignalHistory.csv');
 
-%figure 
-%hold on
+figure 
+hold on
 plot(x(:,2),x(:,3));
 %axis([1.0 1.2 -10.0 10.0])
 axis([0 inf -10.0 10.0])
--- HistoryTest/plot_history34.m
+++ HistoryTest/plot_history34.m
@@ -5,13 +5,15 @@ x = load('C:\Alpha\Data\SignalHistory.csv');
 
 figure('Name','HistoryTest','NumberTitle','off')
 hold on
+plot([min(x(:,2)) max(x(:,2))],[0 0],'k-')
 plot(x(:,2),x(:,3));
 plot(x(:,2),x(:,4));
-axis([0 inf -10.0 10.0])
+axis([0 inf -4.0 4.0])
+
+
 h=zoom;
 set(h,'Motion','horizontal','Enable','on');
 
 
 return
 
-e

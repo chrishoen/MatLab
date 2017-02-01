6079d828dd0e1945baef91299b5f1971a48bcc9f
chrishoen <chris.hoen@red-inc.us>
2017-02-01 18:59:39 -0500

    more

--- HistoryTest/plot_history4.m
+++ HistoryTest/plot_history4.m
@@ -4,7 +4,7 @@ x = load('C:\Alpha\Data\SignalHistory.csv');
 
 figure 
 hold on
-plot(x(:,2),x(:,3));
+plot(x(:,2),x(:,4));
 axis([0 inf -4.0 4.0])
 h=zoom;
 set(h,'Motion','horizontal','Enable','on');

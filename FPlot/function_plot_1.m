ab42a6d623b7204fbd01004f7c734b1243f98ffc
chrishoen <chris.hoen@red-inc.us>
2017-02-06 12:54:34 -0500

    more

--- FPlot/function_plot_1.m
+++ FPlot/function_plot_1.m
@@ -1,10 +1,19 @@
 function function_plot_1
-fplot(@f101,[0 5])
+x = [-5:0.01:5];
+y = myfunction(x);
+
+figure 
+hold on
+plot([min(x) max(x)],[0 0],'k-')
+plot([0 0],[-4 10],'k-')
+plot(x,y);
+axis([-inf inf -4.0 10.0])
+
 return
 
-function y = f101(t)
+function y = myfunction(x)
 
-y = 1/cosh(t);
+y = (x-2).^2 +1;
 
 return
 

c5a077250dea7d044d9058aeff3cd7bda1a86016
chrishoen <chris.hoen@red-inc.us>
2017-04-23 07:44:35 -0400

    more

---
+++ HistoryTest/demo3_plot_345.m
@@ -0,0 +1,22 @@
+function demo3_plot_345
+
+com.mathworks.mlwidgets.html.HtmlComponentFactory.setDefaultType('HTMLRENDERER');
+
+set(0,'DefaultFigureWindowStyle','docked');
+x = load('C:\Alpha\Data\SignalHistory2.csv');
+
+figure('Name','HistoryTest','NumberTitle','off')
+hold on
+plot([min(x(:,2)) max(x(:,2))],[0 0],'k-')
+plot(x(:,2),x(:,3));
+plot(x(:,2),x(:,4));
+plot(x(:,2),x(:,5));
+axis([0 inf -4.0 4.0])
+
+
+h=zoom;
+set(h,'Motion','horizontal','Enable','on');
+
+
+return
+

5e259c264a29a5e343aa11b4f4f215d2b72222f8
chrishoen <chris.hoen@red-inc.us>
2017-04-24 15:11:53 -0400

    more

--- PostAnalyze/SmoothedXY_SS_mult.m
+++ PostAnalyze/SmoothedXY_SS_mult.m
@@ -11,13 +11,14 @@ hold on
 ax1 = subplot(2,1,1);
 ax2 = subplot(2,1,2);
 
-XA=[0 inf -12 12];
+XA1=[0 inf -12 12];
+XA2=[0 inf -12 12];
 
 plot(ax1,x(:,2),x(:,3))
-axis(ax1,XA)
+axis(ax1,XA1)
 
 plot(ax2,x(:,2),x(:,4))
-axis(ax2,XA)
+axis(ax2,XA2)
 
 h=zoom;
 set(h,'Motion','horizontal','Enable','on');

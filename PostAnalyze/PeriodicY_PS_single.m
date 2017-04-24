e39371a765a8bede06f3e24d7f2828c49543b31b
chrishoen <chris.hoen@red-inc.us>
2017-04-24 12:36:44 -0400

    more

---
+++ PostAnalyze/PeriodicY_PS_single.m
@@ -0,0 +1,28 @@
+function PeriodicY_PS_single
+
+com.mathworks.mlwidgets.html.HtmlComponentFactory.setDefaultType('HTMLRENDERER');
+
+x1 = load('C:\RESULTS\History\Periodic.csv');
+x2 = load('C:\RESULTS\History\Smoothed.csv');
+
+set(0,'DefaultFigureWindowStyle','docked')
+figure('Name','PeriodicY_PS','NumberTitle','off')
+hold on
+
+ax1 = subplot(1,1,1);
+ax2 = subplot(1,1,1);
+
+XA1=[0 inf -12 12];
+XA2=[0 inf -12 12];
+
+plot(ax1,x1(:,2),x1(:,4))
+axis(ax1,XA1)
+
+plot(ax2,x2(:,2),x2(:,4))
+axis(ax2,XA2)
+
+h=zoom;
+set(h,'Motion','horizontal','Enable','on');
+
+return
+
---
+++ PostAnalyze/PeriodicY_SD_mult.m
@@ -0,0 +1,28 @@
+function PeriodicY_SD_mult
+
+com.mathworks.mlwidgets.html.HtmlComponentFactory.setDefaultType('HTMLRENDERER');
+
+x1 = load('C:\RESULTS\History\Periodic.csv');
+x2 = load('C:\RESULTS\History\Differentiated.csv');
+
+set(0,'DefaultFigureWindowStyle','docked')
+figure('Name','Periodic_SD','NumberTitle','off')
+hold on
+
+ax1 = subplot(2,1,1);
+ax2 = subplot(2,1,2);
+
+XA1=[0 inf -12 12];
+XA2=[0 inf -12 12];
+
+plot(ax1,x1(:,2),x1(:,4))
+axis(ax1,XA1)
+
+plot(ax2,x2(:,2),x2(:,4))
+axis(ax2,XA2)
+
+h=zoom;
+set(h,'Motion','horizontal','Enable','on');
+
+return
+

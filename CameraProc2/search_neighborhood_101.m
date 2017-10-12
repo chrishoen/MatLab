843ec2ee577afe388dd46df0dd726bc22838465a
chrishoen <chris.hoen@red-inc.us>
2017-10-12 18:35:34 -0400

    more

--- CameraProc2/search_neighborhood_101.m
+++ CameraProc2/search_neighborhood_101.m
@@ -6,15 +6,12 @@ com.mathworks.mlwidgets.html.HtmlComponentFactory.setDefaultType('HTMLRENDERER')
 %*******************************************
 set(0,'DefaultFigureWindowStyle','docked')
 
-X1Select = 3;
+X1Select = 1;
 
 if     (X1Select == 1)
    X1 = imread('C:\Alpha\Image\SearchNeighborhood.png');
    MyPlotName = 'SearchNeighborhood';
 elseif (X1Select == 2)
-   X1 = imread('C:\Alpha\Image\SearchNeighborhoodHigh.png');
-   MyPlotName = 'SearchNeighborhoodHigh';
-elseif (X1Select == 3)
    X1 = imread('C:\Alpha\Image\SearchNeighborhoodLow.png');
    MyPlotName = 'SearchNeighborhoodLow';
 end

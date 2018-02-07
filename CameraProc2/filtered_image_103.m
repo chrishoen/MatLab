5983a68330622a4988622b135e4d950ab5e17ac0
chrishoen <chris.hoen@red-inc.us>
2018-02-07 11:35:59 -0500

    more

--- CameraProc2/filtered_image_103.m
+++ CameraProc2/filtered_image_103.m
@@ -15,8 +15,8 @@ rotate3d on
 
 figure('Name',MyPlotName,'NumberTitle','off');
 
-rowy = 1016;
-colx = 1216;
+rowy = 1016+1;
+colx = 1216+1;
 b = 100;
 
 X2 = X1(rowy-b:rowy+b,colx-b:colx+b);
@@ -26,7 +26,8 @@ X3 = X1(rowy-b:rowy+b,colx:colx);
 %contour3(X1)
 %mesh(X2)
 plot(X3)
-%stem3(X1)
+%stem3(X3)
+stem(X3)
 return
 
 %***********************************************************END

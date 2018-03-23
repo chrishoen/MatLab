f33305c82191e9f895c1e6415d648b6606852ff6
chrishoen <chris.hoen@red-inc.us>
2018-03-23 18:05:41 -0400

    more

---
+++ Simulation/plot_hist_run1_1.m
@@ -0,0 +1,52 @@
+function plot_hist_run1_1
+
+com.mathworks.mlwidgets.html.HtmlComponentFactory.setDefaultType('HTMLRENDERER');
+
+x1 = load('C:\Alpha\Data\SignalHistory1.csv');
+
+set(0,'DefaultFigureWindowStyle','docked')
+figure('Name','plot_hist_run1_2','NumberTitle','off')
+hold on
+
+
+ta1=[0 inf -2 2];
+ta2=[0 inf -2 2];
+ta3=[0 inf -2 2];
+ta4=[0 inf -2 2];
+
+xz = zeros([1 size(x1(:,1))]);
+
+sp1 = subplot(3,1,1);
+hold on
+plot(sp1,x1(:,2),xz)
+plot(sp1,x1(:,2),x1(:,3))
+axis(sp1,ta1)
+title('XX')
+xlabel('Time (sec)')
+ylabel('(units)')
+
+sp2 = subplot(3,1,2);
+hold on
+plot(sp2,x1(:,2),xz)
+plot(sp2,x1(:,2),x1(:,4))
+axis(sp2,ta2)
+title('XV')
+xlabel('Time (sec)')
+ylabel('(units)')
+
+sp3 = subplot(3,1,3);
+hold on
+plot(sp3,x1(:,2),xz)
+plot(sp3,x1(:,2),x1(:,3))
+plot(sp3,x1(:,2),x1(:,4))
+axis(sp3,ta3)
+title('XX+XV')
+xlabel('Time (sec)')
+ylabel('(units/sec)')
+
+linkaxes([sp1,sp2,sp3],'x')
+h=zoom;
+set(h,'Motion','horizontal','Enable','on');
+set(gca,'LooseInset',get(gca,'TightInset'))
+return
+
---
+++ Simulation/plot_hist_run1_2.m
@@ -0,0 +1,35 @@
+function plot_hist_run1_2
+
+com.mathworks.mlwidgets.html.HtmlComponentFactory.setDefaultType('HTMLRENDERER');
+
+x1 = load('C:\Alpha\Data\SignalHistory2.csv');
+
+set(0,'DefaultFigureWindowStyle','docked')
+figure('Name','plot_hist_run1_2','NumberTitle','off')
+hold on
+
+
+ta1=[0 inf -2 2];
+ta2=[0 inf -2 2];
+ta3=[0 inf -2 2];
+ta4=[0 inf -2 2];
+
+xz = zeros([1 size(x1(:,1))]);
+
+sp1 = subplot(3,1,1);
+hold on
+plot(sp1,x1(:,2),xz)
+plot(sp1,x1(:,2),x1(:,3))
+axis(sp1,ta1)
+title('XX')
+xlabel('Time (sec)')
+ylabel('(units)')
+
+
+
+linkaxes([sp1],'x')
+h=zoom;
+set(h,'Motion','horizontal','Enable','on');
+set(gca,'LooseInset',get(gca,'TightInset'))
+return
+

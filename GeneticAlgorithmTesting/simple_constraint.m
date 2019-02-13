6b62f6f849530993df2dde458c297f1bb644fccc
chrishoen <chris123hoen@gmail.com>
2019-02-12 20:08:59 -0500

    cleanup

--- GeneticAlgorithmTesting/simple_constraint.m
+++
@@ -1,9 +0,0 @@
-function [c, ceq] = simple_constraint(x)
-
-%contraint to pass in
-c = [1.5 + x(1)*x(2) + x(1) - x(2);
-    -x(1)*x(2) + 10];
-ceq = [];
-
-end
-
--- GeneticAlgorithmTesting/simple_fitness.m
+++
@@ -1,7 +0,0 @@
-function y = simple_fitness(x)
-
-%Fitness function for ga
-y = 100*(x(1)^2-x(2))^2 + (1-x(1))^2;
-
-end
-
